c6929ff287dd4ce60e811e6d9b1983bee7540c60
Tim Mickel <tmickel@mit.edu>
2012-08-17 15:41:04 -0700

    Fix coverage command in bin/jenkins.sh.
    Jenkins works!

--- bin/jenkins.sh
+++ bin/jenkins.sh
@@ -77,6 +77,6 @@ echo "CREATE DATABASE IF NOT EXISTS ${JOB_NAME}"|mysql -u $DB_USER -h $DB_HOST
 echo "Starting tests..."
 export FORCE_DB=1
 coverage run manage.py test --noinput --with-xunit
-coverage xml $(find apps lib -name '*.py')
+coverage xml $(find airmozilla lib -name '*.py')
 
 echo "FIN"
