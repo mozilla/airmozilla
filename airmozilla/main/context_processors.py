8b7ab303340ba65aa219103c568ce9d88ea39689
Tim Mickel <tmickel@mit.edu>
2012-08-17 16:14:04 -0700

    Fix context processor to correctly display internal featured videos.

--- airmozilla/main/context_processors.py
+++ airmozilla/main/context_processors.py
@@ -4,7 +4,7 @@ from airmozilla.main.models import Event
 
 
 def sidebar(request):
-    featured = Event.objects.approved().filter(public=True, featured=True)
+    featured = Event.objects.approved().filter(featured=True)
     upcoming = Event.objects.upcoming().order_by('start_time')
     if not request.user.is_active:
         featured = featured.filter(public=True)
