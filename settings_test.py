9d14c70b68eb1b00b8b6826ee6fc2e58fb4a0ab6
Peter Bengtsson <mail@peterbe.com>
2012-10-26 13:05:55 -0700

    Use only md5 to hash passwords when running tests

--- settings_test.py
+++ settings_test.py
@@ -1,3 +1,7 @@
 # These settings will always be overriding for all test runs
 
 EMAIL_FROM_ADDRESS = 'doesnt@matter.com'
+
+PASSWORD_HASHERS = (
+    'django.contrib.auth.hashers.MD5PasswordHasher',
+)
