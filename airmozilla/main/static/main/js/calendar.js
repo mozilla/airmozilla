8c49f5fa98c7c91688fd6edc81f4cd0598a9652a
Peter Bengtsson <mail@peterbe.com>
2013-12-11 11:52:24 -0500

    fixes bug 948825 - include weekend for far-east peeps

--- airmozilla/main/static/main/js/calendar.js
+++ airmozilla/main/static/main/js/calendar.js
@@ -1,9 +1,21 @@
 $(function() {
+    var weekends, firstDay = 0;
+    var x = new Date();
+    var currentTimeZoneOffsetInHours = x.getTimezoneOffset() / 60;
+    if (currentTimeZoneOffsetInHours <= -7) {
+        // sufficiently far east that some Friday events might actually
+        // start on a Saturday
+        weekends = true;
+        firstDay = 1;  // far-east people prefer Monday
+    } else {
+        weekends = false;
+    }
     var container = $('#calendar');
     container.fullCalendar({
         events: container.data('events-url'),
         cache: true,
-        weekends: false, // will hide Saturdays and Sundays
+        weekends: weekends,
+        firstDay: firstDay,
         allDaySlot: false,
         defaultEventMinutes: 60,
         ignoreTimezone: false,
