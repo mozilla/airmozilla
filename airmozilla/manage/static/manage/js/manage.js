9fc63de14bbe45aa0fd2a29516ffc5e02a80c525
Peter Bengtsson <mail@peterbe.com>
2013-12-12 14:31:03 -0500

    fixes bug 949492 - event edit thumbnail link to open in new window

--- airmozilla/manage/static/manage/js/manage.js
+++ airmozilla/manage/static/manage/js/manage.js
@@ -22,19 +22,29 @@ function process_vidly_status_response(response, $element) {
 
 $(function() {
 
-  var title = null;
-  if ($('h1:visible').size()) {
-    if ($('h1:visible').size() == 1) {
-      title = $('h1:visible').text();
+    var title = null;
+    if ($('h1:visible').size()) {
+        if ($('h1:visible').size() == 1) {
+            title = $('h1:visible').text();
+        }
+    } else if ($('h2:visible').size()) {
+        if ($('h2:visible').size() == 1) {
+            title = $('h2:visible').text();
+        }
     }
-  } else if ($('h2:visible').size()) {
-    if ($('h2:visible').size() == 1) {
-      title = $('h2:visible').text();
+    if (title) {
+        document.title = title + ' - ' + document.title;
     }
-  }
-  if (title) {
-    document.title = title + ' - ' + document.title;
-  }
 
+    /* The django file widget has a link to the existing image if there is
+     * one. That opens in the same window which increases the risk of
+     * accidentally loading that, going back and losing form changes.
+     */
+    $('.controls a').each(function() {
+        var parent = $(this).parent();
+        if ($('input[type="file"]', parent).length) {
+            $(this).attr('target', '_blank');
+        }
+    });
 
 });
