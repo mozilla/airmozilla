575cf01cc0606e3beae228f426525cfdc13789fa
Peter Bengtsson <mail@peterbe.com>
2014-07-25 12:40:22 -0700

    debug-mozillians tool

---
+++ airmozilla/base/management/commands/debug-mozillians.py
@@ -0,0 +1,35 @@
+import inspect
+from pprint import pprint
+
+from django.core.management.base import BaseCommand, CommandError
+
+from airmozilla.base import mozillians
+
+
+class Command(BaseCommand):  # pragma: no cover
+
+    args = 'command *args'
+
+    def handle(self, *args, **options):
+
+        try:
+            command = args[0]
+            command = getattr(mozillians, command)
+        except (IndexError, AttributeError):
+            functions = (
+                mozillians.is_vouched,
+                mozillians.fetch_user_name,
+                mozillians.in_groups,
+                mozillians.get_all_groups
+            )
+            print "Available commands"
+            for f in functions:
+                print "\t", f.func_name
+                print "\t\t", inspect.getargspec(f)
+            raise CommandError("No command")
+        rest = args[1:]
+
+        # verbosity = int(options['verbosity'])
+        # verbose = verbosity > 1
+        result = command(*rest)
+        pprint(result)
