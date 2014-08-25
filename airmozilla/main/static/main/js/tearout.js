15097180059eefdad68af066c401fe095280f597
Peter Bengtsson <mail@peterbe.com>
2014-08-25 12:26:55 -0700

    fixes bug 1058159 - Placeholder after tear-out pop-up has opened wrong height

--- airmozilla/main/static/main/js/tearout.js
+++ airmozilla/main/static/main/js/tearout.js
@@ -11,6 +11,8 @@ $(function() {
         $('.tearout .warning-about-time').fadeOut(300);
     });
 
+    var player_width = null, player_height = null;
+
     $('.tearout').on('click', 'a.open', function(event) {
         event.preventDefault();
         var iframe = $('.entry-content iframe');
@@ -19,19 +21,24 @@ $(function() {
         var video_url = location.href + 'video/?embedded=false&autoplay=true';
         var video_name = '_blank';
         var features = 'menubar=no,location=no,resizable=no,scrollbars=no,status=no';
-        if (iframe.length) {
-            // probably a vid.ly iframe
-            features += ',width=' + iframe.attr('width');
-            features += ',height=' + iframe.attr('height');
-        } else if (player_wrapper.length) {
-            // probably a live event using jwplayer
-            features += ',height=' + player_wrapper.css('height');
-            features += ',width=' + player_wrapper.css('width');
-        } else {
-            console.warn('No iframe, so hard to guess the width and height of popup');
-            // some sensible defaults
-            features += ',width=640,height=360';
+        if (player_width === null || player_height === null) {
+            if (iframe.length) {
+                // probably a vid.ly iframe
+                player_width = parseInt(iframe.attr('width'), 10);
+                player_height = parseInt(iframe.attr('height'), 10);
+            } else if (player_wrapper.length) {
+                // probably a live event using jwplayer
+                player_width = parseInt(player_wrapper.css('width'), 10);
+                player_height = parseInt(player_wrapper.css('height'), 10);
+            } else {
+                console.warn('No iframe, so hard to guess the width and height of popup');
+                // some sensible defaults
+                player_width = 640;
+                player_height = 360;
+            }
         }
+        features += ',width=' + player_width;
+        features += ',height=' + player_height;
 
         popup = window.open(video_url, video_url, features);
         if (popup === null) {
@@ -57,15 +64,10 @@ $(function() {
             if (placeholder === null) {
                 placeholder = $('<div>')
                     .addClass('tearout-placeholder');
-                if (iframe.length) {
-                    placeholder.css('width', iframe.attr('width'));
-                    placeholder.css('height', iframe.attr('height'));
-                } else if (player_wrapper.length) {
-                    placeholder.css('width', player_wrapper.css('width'));
-                    placeholder.css('height', player_wrapper.css('height'));
-                } else {
-                    placeholder.css('width', 640).css('height', 360);
-                }
+
+                placeholder.css('width', player_width);
+                placeholder.css('height', player_height);
+
                 // and insert a restore link in it
                 $('<a>')
                     .addClass('restore')
