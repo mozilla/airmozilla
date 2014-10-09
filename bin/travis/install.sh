76455513f6de12fff914722dd29daf156f9c808b
Peter Bengtsson <mail@peterbe.com>
2014-10-09 13:04:52 -0700

    install coverage for coveralls to work

--- bin/travis/install.sh
+++ bin/travis/install.sh
@@ -5,7 +5,7 @@ set -e
 echo "Install Python dependencies"
 pip install -r requirements/compiled.txt
 pip install --no-deps -r requirements/dev.txt
-
+pip install coverage
 
 echo "Creating a test database"
 psql -c 'create database airmozilla;' -U postgres
