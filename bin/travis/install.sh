eef6f969ac93083b847c32e35cbe0634de46d3bf
Peter Bengtsson <mail@peterbe.com>
2015-05-22 16:32:56 -0700

    peep hashes fixes

--- bin/travis/install.sh
+++ bin/travis/install.sh
@@ -6,5 +6,9 @@ echo "Install Python dependencies"
 pip install --no-deps -r requirements.txt
 pip install --no-deps -r dev-requirements.txt
 
+# install the same stuff with peep (slow)
+pip install bin/peep-2.4.1.tar.gz
+peep install -r requirements.txt
+
 echo "Creating a test database"
 psql -c 'create database airmozilla;' -U postgres
