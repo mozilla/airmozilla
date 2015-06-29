520e2bd00fc9355802a225d2051450f71b915f7e
Gloria Dwomoh <shekinahglory92@gmail.com>
2015-06-29 16:29:10 -0700

    Sort the events by highest score

--- airmozilla/main/views.py
+++ airmozilla/main/views.py
@@ -1231,15 +1231,17 @@ def related_content(request, slug):
                       .filter(id__in=ids)
 
     curated_groups_map = collections.defaultdict(list)
+    events_list = list(events)
+    events_sorted = sorted(events_list, key=lambda e: ids.index(e.id))
 
     def get_curated_groups(event):
         return curated_groups_map.get('event_id')
 
     context = {
-        'events': events,
+        'events': events_sorted,
         'get_curated_groups': get_curated_groups,
     }
-    print context
+
     return render(request, 'main/es.html', context)
 
 
