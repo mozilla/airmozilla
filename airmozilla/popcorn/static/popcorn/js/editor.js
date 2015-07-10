aa6bae49d24d883927c9cceb09e854b439992e06
Mike Nolan <me@nolski.rocks>
2015-07-10 15:37:09 -0700

    fixes 1187407 - Auto loading popcorn videos into editor

--- airmozilla/popcorn/static/popcorn/js/editor.js
+++ airmozilla/popcorn/static/popcorn/js/editor.js
@@ -1,2 +1,20 @@
-// Initialize the editor with the div id and path to Popcorn Editor.
-PopcornEditor.init('editor', '/static/popcorn/PopcornEditor/editor.html');
+$(document).ready(function () {
+  PopcornEditor.listen(PopcornEditor.events.loaded, function () {
+    $.get($('#editor').data('url'), {
+        slug: $('#editor').data('slug')
+    })
+    .done(function (response) {
+      if(response.data) {
+        PopcornEditor.loadInfo(response.data);
+      } else {
+        PopcornEditor.loadInfo(PopcornEditor.createTemplate(response.metadata));
+      }
+    })
+    .fail(function() {
+      console.warn("Unable to load popcorn data :(");
+      console.error.apply(console, arguments);
+    });
+  })
+  // Initialize the editor with the div id and path to Popcorn Editor.
+  PopcornEditor.init('editor', '/static/popcorn/PopcornEditor/editor.html');
+});
