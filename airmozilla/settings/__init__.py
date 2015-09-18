a5efe88eaa74449630d308a9afa7c9e40745c8d8
Peter Bengtsson <mail@peterbe.com>
2015-09-18 09:53:48 -0700

    fixes bug 1011881 - re-jig the raven error into a warning

--- airmozilla/settings/__init__.py
+++ airmozilla/settings/__init__.py
@@ -1,10 +1,20 @@
 from .base import *
 try:
     from .local import *
-except ImportError, exc:
+except ImportError, exc:  # pragma: no cover
     exc.args = tuple([
         '%s (did you rename airmozilla/settings/local.py-dist?)' % (
             exc.args[0],
         )
     ])
     raise exc
+
+
+# This takes care of removing
+try:
+    assert RAVEN_CONFIG['dsn']
+except (NameError, KeyError, AssertionError):  # pragma: no cover
+    # Then it's not been configured!
+    INSTALLED_APPS = list(INSTALLED_APPS)
+    INSTALLED_APPS.remove('raven.contrib.django.raven_compat')
+    INSTALLED_APPS = tuple(INSTALLED_APPS)
